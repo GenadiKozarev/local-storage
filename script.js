2d1c41a12ba945686f2b3f4cd890dc45b54bde82
gkoz <genadi.kozarev+github@gmail.com>
2024-11-15 17:55:28 +0000

    populates the list with items and adds an option to mark them as 'done'

--- script.js
+++ script.js
@@ -11,8 +11,24 @@ function addItem(e) {
         done: false,
     };
     items.push(item);
+    populateList(items, itemsList);
     // empties the text input value
     this.reset();
 }
+
+const populateList = (plates = [], platesList) => {
+    platesList.innerHTML = plates
+        .map((plate, index) => {
+            return `
+            <li>
+                <input type="checkbox" data-index=${index} id="item${index}" ${
+                plate.done ? 'checked' : ''
+            } />
+                <label for="item${index}">${plate.title}</label>
+            </li>
+        `;
+        })
+        .join('');
+};
 
 addItems.addEventListener('submit', addItem);
